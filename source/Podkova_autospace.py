1d13f662c1a7f4207b99f10d5015bb9dbd490421
Alexei Vanyashin <a@cyreal.org>
2016-11-04 18:30:04 -0400

    added custom HTSpace params paraArea=400 etc

--- source/Podkova_autospace.py
+++ source/Podkova_autospace.py
@@ -2,13 +2,11 @@
 # Reference
 # Script, Category, Subcategory, value, referenceGlyph, filter
 *,Letter,Uppercase,1.25,H,*,
-*,Letter,Smallcaps,1.1,h.sc,*,
 *,Letter,Lowercase,1,x,*,
 *,Letter,Lowercase,0.7,m.sups,.sups,
 
 # Numbers
 *,Number,Decimal Digit,1.2,one,*,
-*,Number,Decimal Digit,1.2,zero.osf,.osf,
 *,Number,Fraction,1.3,*,*,
 *,Number,*,0.8,*,.dnom,
 *,Number,*,0.8,*,.numr,
@@ -27,7 +25,3 @@
 *,Symbol,Currency,1.6,*,*,
 *,Symbol,*,1.5,*,*,
 *,Mark,*,1,*,*,
-
-# Devanagari
-devanagari,Letter,Other,1,devaHeight,*,
-devanagari,Letter,Ligature,1,devaHeight,*,
